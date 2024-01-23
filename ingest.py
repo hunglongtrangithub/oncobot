962d89acbb9313f10ccf033c97fb848d93363dcb
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-01-22 23:16:29 -0500

    feat(ingest.py): change vectorstore from Weaviate to FAISS
    Save the embeddings to a local FAISS index in faiss_index
    
    
    Former-commit-id: 0ce8de07bc2ac8d5a50a7522d8e38afd27b53e5e

--- ingest.py
+++ ingest.py
@@ -2,27 +2,19 @@
 import logging
 import os
 import re
-from parser import langchain_docs_extractor
+from parse import langchain_docs_extractor
 
-import weaviate
 from bs4 import BeautifulSoup, SoupStrainer
 from langchain.document_loaders import RecursiveUrlLoader, SitemapLoader
-from langchain.indexes import SQLRecordManager
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema.embeddings import Embeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.utils.html import (PREFIXES_TO_IGNORE_REGEX,
-                                  SUFFIXES_TO_IGNORE_REGEX)
-from langchain.vectorstores.weaviate import Weaviate
+from langchain.utils.html import PREFIXES_TO_IGNORE_REGEX, SUFFIXES_TO_IGNORE_REGEX
+from langchain.vectorstores.faiss import FAISS
 
-from _index import index
-from chain import get_embeddings_model
-from constants import WEAVIATE_DOCS_INDEX_NAME
+from langchain.embeddings.voyageai import VoyageEmbeddings
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-WEAVIATE_URL = os.environ["WEAVIATE_URL"]
-WEAVIATE_API_KEY = os.environ["WEAVIATE_API_KEY"]
-RECORD_MANAGER_DB_URL = os.environ["RECORD_MANAGER_DB_URL"]
 
 
 def metadata_extractor(meta: dict, soup: BeautifulSoup) -> dict:
@@ -53,23 +45,6 @@ def load_langchain_docs():
     ).load()
 
 
-def load_langsmith_docs():
-    return RecursiveUrlLoader(
-        url="https://docs.smith.langchain.com/",
-        max_depth=8,
-        extractor=simple_extractor,
-        prevent_outside=True,
-        use_async=True,
-        timeout=600,
-        # Drop trailing / to avoid duplicate pages.
-        link_regex=(
-            f"href=[\"']{PREFIXES_TO_IGNORE_REGEX}((?:{SUFFIXES_TO_IGNORE_REGEX}.)*?)"
-            r"(?:[\#'\"]|\/[\#'\"])"
-        ),
-        check_response_status=True,
-    ).load()
-
-
 def simple_extractor(html: str) -> str:
     soup = BeautifulSoup(html, "lxml")
     return re.sub(r"\n\n+", "\n\n", soup.text).strip()
@@ -96,17 +71,21 @@ def load_api_docs():
     ).load()
 
 
+def get_embeddings_model() -> Embeddings:
+    if os.environ.get("VOYAGE_API_KEY") and os.environ.get("VOYAGE_AI_MODEL"):
+        return VoyageEmbeddings(model=os.environ["VOYAGE_AI_MODEL"])
+    return OpenAIEmbeddings(chunk_size=200)
+
+
 def ingest_docs():
     docs_from_documentation = load_langchain_docs()
     logger.info(f"Loaded {len(docs_from_documentation)} docs from documentation")
     docs_from_api = load_api_docs()
     logger.info(f"Loaded {len(docs_from_api)} docs from API")
-    docs_from_langsmith = load_langsmith_docs()
-    logger.info(f"Loaded {len(docs_from_langsmith)} docs from Langsmith")
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=200)
     docs_transformed = text_splitter.split_documents(
-        docs_from_documentation + docs_from_api + docs_from_langsmith
+        docs_from_documentation + docs_from_api
     )
 
     # We try to return 'source' and 'title' metadata when querying vector store and
@@ -118,39 +97,9 @@ def ingest_docs():
         if "title" not in doc.metadata:
             doc.metadata["title"] = ""
 
-    client = weaviate.Client(
-        url=WEAVIATE_URL,
-        auth_client_secret=weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY),
-    )
     embedding = get_embeddings_model()
-    vectorstore = Weaviate(
-        client=client,
-        index_name=WEAVIATE_DOCS_INDEX_NAME,
-        text_key="text",
-        embedding=embedding,
-        by_text=False,
-        attributes=["source", "title"],
-    )
-
-    record_manager = SQLRecordManager(
-        f"weaviate/{WEAVIATE_DOCS_INDEX_NAME}", db_url=RECORD_MANAGER_DB_URL
-    )
-    record_manager.create_schema()
-
-    indexing_stats = index(
-        docs_transformed,
-        record_manager,
-        vectorstore,
-        cleanup="full",
-        source_id_key="source",
-        force_update=(os.environ.get("FORCE_UPDATE") or "false").lower() == "true",
-    )
-
-    logger.info(f"Indexing stats: {indexing_stats}")
-    num_vecs = client.query.aggregate(WEAVIATE_DOCS_INDEX_NAME).with_meta_count().do()
-    logger.info(
-        f"LangChain now has this many vectors: {num_vecs}",
-    )
+    vectorstore = FAISS.from_documents(docs_transformed, embedding)
+    vectorstore.save_local("faiss_index")
 
 
 if __name__ == "__main__":

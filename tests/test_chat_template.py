56ab7e8dc54e7d6ad348d16594a5ae5180b4769b
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-03-14 11:29:23 -0400

    test: compare the chat template from the local jinja file with the model's default chat template
    Former-commit-id: 4cb4144783ff8d67959cdeb0b2543db43b620b3b

--- tests/test_chat_template.py
+++ tests/test_chat_template.py
@@ -1,5 +1,9 @@
 from jinja2 import Environment, FileSystemLoader
 import os
+from transformers import AutoTokenizer
+
+checkpoint = "meta-llama/Llama-2-7b-chat-hf"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
 # Create a new Environment object with the FileSystemLoader
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -10,6 +14,7 @@ template = env.get_template("chat_template.j2")
 
 # Your list of messages
 messages = [
+    # {"role": "system", "content": "Welcome to the chat!"},
     {"role": "user", "content": "Hello"},
     {"role": "assistant", "content": "Hi"},
     {"role": "user", "content": "How are you?"},
@@ -21,7 +26,14 @@ messages = [
 
 # Render the template with the messages
 result = template.render(
-    messages=messages, bos_token="[BOS]", eos_token="[EOS]", raise_exception=Exception
+    messages=messages, bos_token="<s>", eos_token="</s>", raise_exception=Exception
 )
 
+# Compare with the output from the tokenizer
+tokenized_chat = tokenizer.apply_chat_template(
+    messages, tokenize=True, add_generation_prompt=True, return_tensors="pt"
+)
+chat = tokenizer.decode(tokenized_chat[0])
+
 print(result)
+# print(chat)

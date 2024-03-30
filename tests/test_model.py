ea8ec452aec8e54509ddbca433e38428ce4d93cd
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-03-30 10:50:14 -0400

    tests: add test for CustomChatGroq model
    Former-commit-id: 47307917dc00dddf67a91dabe68fcde1d9d2c59f

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,6 +7,7 @@ from custom_chat_model import (
     CustomChatHuggingFace,
     CustomChatLlamaReplicate,
     CustomChatOpenAI,
+    CustomChatGroq,
 )
 
 checkpoint = "facebook/opt-125m"
@@ -124,11 +125,50 @@ def test_openai_model(stream=False):
     asyncio.run(stream_async())
 
 
+def test_groq_model(stream=False):
+    model = CustomChatGroq()
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a friendly chatbot who always responds in the style of a pirate",
+        },
+        {
+            "role": "user",
+            "content": "An increasing sequence from 1 to 10:",
+        },
+    ]
+    print("Invoke:")
+    print(model.invoke(messages))
+
+    print("Stream:")
+    for token in model.stream(messages):
+        print(token, end="", flush=True)
+    print()
+
+    print("Invoke with async:")
+
+    async def invoke_async():
+        print(await model.ainvoke(messages))
+
+    asyncio.run(invoke_async())
+
+    print("Stream with async:")
+
+    async def stream_async():
+        async for token in model.astream(messages):
+            print(token, end="", flush=True)
+        print()
+
+    asyncio.run(stream_async())
+
+
 if __name__ == "__main__":
-    print("Testing Hugging Face model")
-    test_hf_model()
-    print("Testing Llama model")
+    # print("Testing Hugging Face model")
+    # test_hf_model()
+    # print("Testing Llama model")
     # test_llama_model()
     # print("Testing OpenAI model")
     # test_openai_model()
+    print("Testing Groq model")
+    test_groq_model()
     print("All tests passed")

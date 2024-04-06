800defec0f506e7def4f02b422e7466b1c679714
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-04-06 13:49:43 -0400

    tests: add tests in test_hf.py
    Former-commit-id: c113f51960be6ac4c9a601793e5a3fb37847321f

--- tests/test_hf.py
+++ tests/test_hf.py
@@ -1,7 +1,9 @@
+import torch
 from threading import Thread
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
+    BitsAndBytesConfig,
     TextIteratorStreamer,
     pipeline,
 )
@@ -15,22 +17,41 @@ checkpoint = "meta-llama/Llama-2-7b-chat-hf"
 
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
-messages = [
-    {
-        "role": "assistant",
-        "content": "You are a friendly chatbot who always responds in the style of a pirate",
-    },
-    {
-        "role": "user",
-        "content": "How many helicopters can a human eat in one sitting?",
-    },
-]
 
-conversation_string = tokenizer.apply_chat_template(
-    messages,
-    add_generation_prompt=True,
-    tokenize=False,
-    return_tensors="pt",
+def test_chat_template():
+    messages = [
+        {
+            "role": "assistant",
+            "content": "You are a friendly chatbot who always responds in the style of a pirate",
+        },
+        {
+            "role": "user",
+            "content": "How many helicopters can a human eat in one sitting?",
+        },
+    ]
+
+    conversation_string = tokenizer.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=False,
+        return_tensors="pt",
+    )
+    print(tokenizer.default_chat_template)
+    print(conversation_string)
+
+
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    # bnb_4bit_use_double_quant=True,
+    # bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
 )
-print(tokenizer.default_chat_template)
-print(conversation_string)
+model = AutoModelForCausalLM.from_pretrained(
+    checkpoint,
+    device_map="auto",
+    quantization_config=quantization_config,
+)
+print(model.get_memory_footprint() / 1024**3)  # in GB
+# generate some text
+generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+print(generator("Hello, how are you?", max_length=100)[0]["generated_text"])

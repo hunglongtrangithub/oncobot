d996ffb276e9e1e4e0c96694a0c45dcd3d0f16ac
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-04-15 16:28:59 -0400

    update test_hf.py
    Former-commit-id: d2ab58d09c0cd67a51cfcb105ab08d66520c1e06

--- tests/test_hf.py
+++ tests/test_hf.py
@@ -40,18 +40,31 @@ def test_chat_template():
     print(conversation_string)
 
 
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    # bnb_4bit_use_double_quant=True,
-    # bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16,
-)
-model = AutoModelForCausalLM.from_pretrained(
-    checkpoint,
-    device_map="auto",
-    quantization_config=quantization_config,
-)
-print(model.get_memory_footprint() / 1024**3)  # in GB
-# generate some text
-generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
-print(generator("Hello, how are you?", max_length=100)[0]["generated_text"])
+# quantization_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     # bnb_4bit_use_double_quant=True,
+#     # bnb_4bit_quant_type="nf4",
+#     bnb_4bit_compute_dtype=torch.bfloat16,
+# )
+# model = AutoModelForCausalLM.from_pretrained(
+#     checkpoint,
+#     device_map="auto",
+#     quantization_config=quantization_config,
+# )
+# print(model.get_memory_footprint() / 1024**3)  # in GB
+# # generate some text
+# generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+# print(generator("Hello, how are you?", max_length=100)[0]["generated_text"])
+
+
+def test_login():
+    from config import settings
+    from huggingface_hub import login
+
+    token = settings.hf_token.get_secret_value()
+    login(token=token)
+
+
+if __name__ == "__main__":
+    # test_chat_template()
+    test_login()

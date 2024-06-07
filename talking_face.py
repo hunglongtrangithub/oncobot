c5c2c6cefcdc699281b3be95c5cc929c4a6a1e89
Hung Tran <hunglongtran2004@gmail.com>
2024-06-07 13:08:39 -0400

    feat(talking_face): implement CustomSadTalker to usee in fastapi endpoint
    Former-commit-id: 9e011ea2f860758dc266ad9df857f3f67bbc6ba7

--- talking_face.py
+++ talking_face.py
@@ -1,11 +1,8 @@
-import os
 from pathlib import Path
 import shutil
 from logger_config import get_logger
 
-# from sad_talker import SadTalker
-
-# print("SadTalker imported")
+from sad_talker import SadTalker
 
 logger = get_logger(__name__)
 
@@ -18,23 +15,59 @@ class DummyTalker:
         # copy video/chatbot1__1.mp4 to video_path
         shutil.copy(Path(__file__).parent / "video/chatbot1__1.mp4", video_path)
         logger.info(f"Created video {video_path}")
-        # # combine audio_path and video_path to create video with audio
-        # codec = "copy"
-        # output_path = "output.mp4"
-        # cmd = f"ffmpeg -i {video_path} -i {audio_path} -c:v {codec} -c:a aac {output_path}"
-        # os.system(cmd)
-        # logger.info(f"Created video with audio")
 
     async def arun(self, video_path: str, audio_path: str, image_path: str):
         self.run(video_path, audio_path, image_path)
 
 
-# TODO: Implement SadTalker
+class CustomSadTalker(SadTalker):
+    def __init__(self):
+        checkpoint_path = Path(__file__).parent / "sad_talker/checkpoints"
+        config_path = Path(__file__).parent / "sad_talker/src/config"
+        super().__init__(
+            checkpoint_path=str(checkpoint_path),
+            config_path=str(config_path),
+        )
+        logger.info(f"CustomSadTalker initialized.")
+
+    def run(
+        self,
+        video_path: str,
+        audio_path: str,
+        image_path: str,
+        delete_generated_files=True,
+    ):
+        logger.info(f"Generating video at {video_path}")
+        try:
+            result_dir = str(Path(__file__).parent / "video")
+            returned_video_path = super().test(
+                source_image=image_path,
+                driven_audio=audio_path,
+                result_dir=result_dir,
+            )
+
+            # copy returned_video_path to video_path
+            shutil.copy(returned_video_path, video_path)
+            if delete_generated_files:
+                # delete returned_video_path's parent directory
+                shutil.rmtree(Path(returned_video_path).parent)
+                logger.info(
+                    f"Deleted generated files at {Path(returned_video_path).parent}"
+                )
+
+            logger.info(f"Created video at {video_path}")
+        except Exception as e:
+            error_message = f"Failed to generate video: {e}"
+            logger.error(error_message)
+            raise Exception(error_message)
+
+    async def arun(self, video_path: str, audio_path: str, image_path: str):
+        self.run(video_path, audio_path, image_path)
+
 
 if __name__ == "__main__":
-    talker = DummyTalker()
-    talker.run(
-        video_path="video/chatbot1.mp4",
-        audio_path="video/chatbot1.mp3",
-        image_path="faces/chatbot1.jpg",
-    )
+    talker = CustomSadTalker()
+    video_path = str(Path(__file__).parent / "video/chatbot2__1.mp4")
+    audio_path = str(Path(__file__).parent / "voices/chatbot2.mp3")
+    image_path = str(Path(__file__).parent / "faces/chatbot2.jpg")
+    talker.run(video_path, audio_path, image_path)

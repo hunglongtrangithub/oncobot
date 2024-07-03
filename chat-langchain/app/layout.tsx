8a5cbe9cec3f4ff4fa9ca7daa1f19b3b895b7ec2
Hung Tran <hunglongtran2004@gmail.com>
2024-07-03 17:51:11 -0400

    feat(chat-langchain): change favicon + web metadata
    Former-commit-id: ca574e2a114a5d231f32072c325bc3d2238214e9

--- chat-langchain/app/layout.tsx
+++ chat-langchain/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chat LangChain",
-  description: "Chatbot for LangChain",
+  title: "Medical Chatbot",
+  description: "Chatbot for Medical Question Answering",
 };
 
 export default function RootLayout({

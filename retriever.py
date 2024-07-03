e2e5bcce9ae76a91b649c412eef4d1dca2113e29
Hung Tran <hunglongtran2004@gmail.com>
2024-07-03 16:55:56 -0400

    feat(retriever): apply some normalizing to query text + LCS comparison for retrieved doc titles
    Former-commit-id: afee420c4aef7126a089df585fbffd8718d6b0b5

--- retriever.py
+++ retriever.py
@@ -14,6 +14,36 @@ class Document(BaseModel):
     source: str
 
 
+def longest_common_substring(s1, s2):
+    n = len(s1)
+    m = len(s2)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    max_length = 0
+    ending_index_s1 = -1
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                if dp[i][j] > max_length:
+                    max_length = dp[i][j]
+                    ending_index_s1 = i - 1
+            else:
+                dp[i][j] = 0
+
+    # Extract the longest common substring using the ending index and max_length
+    # print(s1)
+    # print(s2)
+    if max_length <= 0:
+        print("No common substring found")
+        return "", 0
+    longest_common_substring = s1[
+        ending_index_s1 - max_length + 1 : ending_index_s1 + 1
+    ]
+    # print(f"Longest common substring: {longest_common_substring}")
+    return longest_common_substring, max_length
+
+
 class CustomRetriever:
     def __init__(self, num_docs: int = 5, semantic_ratio: float = 0.1):
         self.index_name = INDEX_NAME
@@ -34,6 +64,8 @@ class CustomRetriever:
             },
             "limit": self.num_docs,
         }
+        query = "".join(query.strip().lower().split())
+        logger.info(f"Searching documents for query: {query}")
         try:
             result = self.index.search(query, opt_params)
         except MeilisearchApiError as e:
@@ -41,7 +73,16 @@ class CustomRetriever:
             return []
 
         docs = result["hits"]
-        logger.info(f"Found {len(docs)} documents.")
+        docs = sorted(
+            docs,
+            key=lambda doc: longest_common_substring(
+                query,
+                "".join(doc["title"].lower().split("_")),
+                # doc["title"].lower(),
+            )[1],
+            reverse=True,
+        )
+        logger.info(f"Found {len(docs)} documents: {[doc['title'] for doc in docs]}")
         return [
             Document(
                 page_content=doc["page_content"],
@@ -57,7 +98,8 @@ class CustomRetriever:
 
 if __name__ == "__main__":
     retriever = CustomRetriever(num_docs=5)
-    query = "What is Fake Patient1 diagnosed with?"
+    # query = "What is fake patient 1 diagnosed with?"
+    query = "fake patient 1"
     docs = retriever.get_relevant_documents(query)
     for doc in docs:
         print(doc.title)

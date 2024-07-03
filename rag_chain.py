85e0b823e37c71b55448e7a8d551992fb6bae1de
Hung Tran <hunglongtran2004@gmail.com>
2024-07-03 17:00:57 -0400

    feat(rag_chain): improve prompt templates
    Temporarily comment out chat logs. Also remove the NER-formatted text from the system prompt for now.
    
    
    Former-commit-id: dcfc391cbd06064bd4dae865a7b0f6f30a698dcd

--- rag_chain.py
+++ rag_chain.py
@@ -1,57 +1,57 @@
 from jinja2 import Template
 from typing import List, Optional, Dict, Tuple, Union, Generator, AsyncGenerator
 from pydantic import BaseModel, Field
-from transformers import pipeline
 
 from logger_config import get_logger
 from retriever import CustomRetriever, Document
 from custom_chat_model import BaseChat
+from ner import NERProcessor
 import logging
 
 logger = get_logger(__name__)
 
 
-# System prompt for medical chatbot
-SYSTEM_TEMPLATE = """\
-You are a medical assistant, tasked with answering any question about the provided \
-medical context.
+# # System prompt for medical chatbot
+# SYSTEM_TEMPLATE = """\
+# You are a medical assistant, tasked with answering any question about the provided \
+# medical context.
 
-Generate a comprehensive and informative answer of 80 words or less for the \
-given question based solely on the provided search results. You must \
-only use information from the provided search results. Use an unbiased and \
-journalistic tone. Combine search results together into a coherent answer. Do not \
-repeat text. Cite search results using [${{number}}] notation. Only cite the most \
-relevant results that answer the question accurately. Place these citations at the end \
-of the sentence or paragraph that reference them - do not put them all at the end. If \
-different results refer to different entities within the same name, write separate \
-answers for each entity.
+# Generate a comprehensive and informative answer of 80 words or less for the \
+# given question based solely on the provided search results. You must \
+# only use information from the provided search results. Use an unbiased and \
+# journalistic tone. Combine search results together into a coherent answer. Do not \
+# repeat text. Cite search results using [${{number}}] notation. Only cite the most \
+# relevant results that answer the question accurately. Place these citations at the end \
+# of the sentence or paragraph that reference them - do not put them all at the end. If \
+# different results refer to different entities within the same name, write separate \
+# answers for each entity.
 
-You should use bullet points in your answer for readability. Put citations where they apply
-rather than putting them all at the end.
+# You should use bullet points in your answer for readability. Put citations where they apply
+# rather than putting them all at the end.
 
-If there is nothing in the context relevant to the question at hand, just say "Hmm, \
-I'm not sure." Don't try to make up an answer.
+# If there is nothing in the context relevant to the question at hand, just say "Hmm, \
+# I'm not sure." Don't try to make up an answer.
 
-If there is no relevant information within the context, just say "Hmm, I'm not sure." \
-Do not make up an answer.
+# If there is no relevant information within the context, just say "Hmm, I'm not sure." \
+# Do not make up an answer.
 
-Anything between the following `context`  html blocks is retrieved from a knowledge \
-bank, not part of the conversation with the user. 
+# Anything between the following `context`  html blocks is retrieved from a knowledge \
+# bank, not part of the conversation with the user.
 
-<context>
-    {context} 
-<context/>
+# <context>
+#     {context}
+# <context/>
 
-REMEMBER: If there is no relevant information within the context, just say "Hmm, I'm \
-not sure." Don't try to make up an answer. Anything between the preceding 'context' \
-html blocks is retrieved from a knowledge bank, not part of the conversation with the \
-user. Always answer in 80 words or less.\
-"""
+# REMEMBER: If there is no relevant information within the context, just say "Hmm, I'm \
+# not sure." Don't try to make up an answer. Anything between the preceding 'context' \
+# html blocks is retrieved from a knowledge bank, not part of the conversation with the \
+# user. Always answer in 80 words or less.\
+# """
 
-# System prompt that includes NER results
+# System prompt for one medical document
 SYSTEM_TEMPLATE = """\
 You are a medical assistant tasked with answering any question about the provided \
-context, which includes a medical document and its NER information.
+medical context,
 
 Generate a comprehensive and informative answer of 80 words or less for the given \
 question based solely on the provided context. Use an unbiased and journalistic tone. \
@@ -70,29 +70,58 @@ bank, not part of the conversation with the user.
 REMEMBER: If there is no relevant information within the context, just say "Hmm, I'm \
 not sure." Always answer in 80 words or less.\
 """
+SYSTEM_TEMPLATE = """\
+You are a virtual medical assistant tasked with providing answers to questions about a specific patient. Use the documents provided in the context to find information relevant to the patient mentioned in the question.
 
+Instructions:
+- Identify and extract relevant information about the patient from the context.
+- Answer the user's question using this information.
+- If the context does not contain information relevant to the patient or the question, respond with "Hmm, I'm not sure."
+- Keep answers concise and to the point, not exceeding 80 words. Use an unbiased, journalistic tone.
+- Avoid repeating text directly from the context and do not fabricate any information.
 
-# Template for asking the model to extract the name of the patient
-REPHRASE_TEMPLATE = """
-Given the following chat history:
+<context>
+    {context}
+</context>
 
-{chat_history}
-
-And the follow-up question:
-
-{question}
-
-Extract the name of the patient that is the most relevant to the conversation leading up to the follow-up question. Do not \
-include any other information in your response.
+REMEMBER: Your answers should be based solely on the provided documents about the patient. If relevant information is not available, state "Hmm, I'm not sure." Always answer in 80 words or less.
 """
 
+
+# # Template for asking the model to extract the name of the patient
+# REPHRASE_TEMPLATE = """
+# Given the following conversation:
+
+# {chat_history}
+
+# And the follow-up question:
+# ###
+# {question}
+# ###
+# Extract the patient name that is the most relevant to the follow-up question. Do not \
+# include any other information in your response.
+# """
+
+# Systemp prompt for extracting the name of the patient
+SYSTEM_REPHRASE_PROMPT = """\
+You are a medical assistant tasked with extracting the name of the patient that is most \
+relevant to the follow-up question.
+"""
+REPHRASE_TEMPLATE = """
+Given the following question:
+###
+{question}
+###
+Extract the patient name that is the most relevant to the question. Do not \
+include any other information in your response.
+"""
 # Jinja2 template for formatting chat history
 CHAT_TEMPLATE_STRING = """\
 {%- for message in messages %}
 {% if message.role == 'user' -%}
-Human: {{ message.content -}}\n
+###Human###: {{ message.content -}}\n
 {%- elif message.role == 'assistant' -%}
-AI: {{ message.content -}}\n
+###AI###: {{ message.content -}}\n
 {%- else -%}
 Unknown Role: {{ message.content -}}\n
 {%- endif -%}
@@ -120,10 +149,15 @@ class ChatRequest(BaseModel):
 
 
 class RAGChain:
-    def __init__(self, retriever: CustomRetriever, chat_llm: BaseChat):
+    def __init__(
+        self,
+        retriever: CustomRetriever,
+        chat_llm: BaseChat,
+        ner: NERProcessor,
+    ):
         self.chat_llm = chat_llm
         self.retriever = retriever
-        self.ner = pipeline("token-classification", model="d4data/biomedical-ner-all")
+        self.ner = ner
         self.chat_logger = logging.getLogger(__name__)
         self.chat_logger.setLevel(logging.INFO)
         handler = logging.FileHandler("chat_history.log")
@@ -150,73 +184,6 @@ class RAGChain:
 
         return formatted_chat_history
 
-    def get_serialized_entities(self, text: str) -> str:
-        try:
-            entities = self.ner(text, grouped_entities=True)
-        except Exception as e:
-            logger.error(f"Error in NER Engine: {e}")
-            entities = []
-
-        if len(entities) == 0:  # type: ignore
-            entity_list = [(text, None, 0)]
-        else:
-            list_format = []
-            index = 0
-            entities = sorted(entities, key=lambda x: x["start"])  # type: ignore
-            for entity in entities:
-                list_format.append((text[index : entity["start"]], None, index))
-                entity_category = entity.get("entity") or entity.get("entity_group")
-                list_format.append(
-                    (
-                        text[entity["start"] : entity["end"]],
-                        entity_category,
-                        entity["start"],
-                    )
-                )
-                index = entity["end"]
-            list_format.append((text[index:], None, index))
-            entity_list = list_format
-
-        output = []
-        running_text, running_category, running_start = None, None, None
-        # NOTE: This is a hack to avoid the adjacent_separator
-        self.adjacent_separator = ""
-        for text, category, start in entity_list:
-            if running_text is None:
-                running_text = text
-                running_category = category
-                running_start = start
-            elif category == running_category:
-                running_text += self.adjacent_separator + text
-            elif not text:
-                # Skip fully empty item, these get added in processing
-                # of dictionaries.
-                pass
-            else:
-                output.append((running_text, running_category, running_start))
-                running_text = text
-                running_category = category
-                running_start = start
-        if running_text is not None:
-            output.append((running_text, running_category, running_start))
-
-        # group the entities by category
-        entity_dict = {}
-        for text, category, start in output:
-            if not category:
-                continue
-            category = category.replace("_", " ")
-            if category in entity_dict:
-                entity_dict[category].append(str((text, start)))
-            else:
-                entity_dict[category] = [str((text, start))]
-
-        # serialize the entities
-        serialized_entities = "NER Entities:\n"
-        for category, entities in entity_dict.items():
-            serialized_entities += f"{category}: {', '.join(entities)}\n"
-        return serialized_entities
-
     async def aretrieve_documents(self, request: ChatRequest) -> List[Document]:
         chat_history = self.format_chat_history(request.chat_history)
         query_question = request.question
@@ -231,9 +198,13 @@ class RAGChain:
         rephrase_question_prompt = REPHRASE_TEMPLATE.format(
             chat_history=serialized_chat_history, question=query_question
         )
+        # self.chat_logger.info(f"Rephrased question prompt: {rephrase_question_prompt}")
         try:
             rephrased_question = await self.chat_llm.ainvoke(
-                [{"role": "user", "content": rephrase_question_prompt}],
+                [
+                    {"role": "system", "content": SYSTEM_REPHRASE_PROMPT},
+                    {"role": "user", "content": rephrase_question_prompt},
+                ],
             )
             query_question = rephrased_question.strip()
             self.chat_logger.info(f"Rephrased question: {query_question}")
@@ -265,7 +236,10 @@ class RAGChain:
         )
         try:
             rephrased_question = self.chat_llm.invoke(
-                [{"role": "user", "content": rephrase_question_prompt}],
+                [
+                    {"role": "system", "content": SYSTEM_REPHRASE_PROMPT},
+                    {"role": "user", "content": rephrase_question_prompt},
+                ],
             )
             query_question = rephrased_question.strip()
             self.chat_logger.info(f"Rephrased question: {query_question}")
@@ -284,14 +258,12 @@ class RAGChain:
     def get_response_streamer_with_docs(
         self, request: ChatRequest, docs: List[Document]
     ) -> Generator[str, None, None]:
-        docs = docs[:1]  # Only use the first document for now
+        # docs = docs[:1]  # Only use the first document for now
         serialized_docs = "\n".join(
             [
                 f"<doc id='{i}'>"
-                + doc.page_content
-                + "\n"
-                + self.get_serialized_entities(doc.page_content)
-                + "</doc>"
+                # + self.ner.get_ner_inserted_text(doc.page_content.strip())
+                + doc.page_content.strip() + "</doc>"
                 for i, doc in enumerate(docs)
             ]
         )
@@ -303,7 +275,7 @@ class RAGChain:
             *formatted_chat_history,
             {"role": "user", "content": request.question},
         ]
-        self.chat_logger.info(f"System prompt:\n{system_prompt}")
+        # self.chat_logger.info(f"System prompt:\n{system_prompt}")
         try:
             text_streamer = self.chat_llm.stream(current_conversation)
             return text_streamer  # type: ignore
@@ -314,14 +286,12 @@ class RAGChain:
     async def aget_response_streamer_with_docs(
         self, request: ChatRequest, docs: List[Document]
     ) -> AsyncGenerator[str, None]:
-        docs = docs[:1]  # Only use the first document for now
+        # docs = docs[:1]  # Only use the first document for now
         serialized_docs = "\n".join(
             [
                 f"<doc id='{i}'>"
-                + doc.page_content
-                + "\n\n"
-                + self.get_serialized_entities(doc.page_content)
-                + "</doc>"
+                # + self.ner.get_ner_inserted_text(doc.page_content.strip())
+                + doc.page_content.strip() + "</doc>"
                 for i, doc in enumerate(docs)
             ]
         )
@@ -330,10 +300,10 @@ class RAGChain:
         formatted_chat_history = self.format_chat_history(request.chat_history)
         current_conversation = [
             {"role": "system", "content": system_prompt},
-            *formatted_chat_history,
+            # *formatted_chat_history,
             {"role": "user", "content": request.question},
         ]
-        self.chat_logger.info(f"System prompt:\n{system_prompt}")
+        # self.chat_logger.info(f"System prompt:\n{system_prompt}")
         try:
             text_streamer = await self.chat_llm.astream(current_conversation)
             return text_streamer
@@ -414,7 +384,8 @@ if __name__ == "__main__":
 
     retriever = CustomRetriever()
     chat_llm = CustomChatHuggingFace()
-    rag_chain = RAGChain(retriever, chat_llm)
+    ner = NERProcessor()
+    rag_chain = RAGChain(retriever, chat_llm, ner)
     # text = open("./docs/fake_patient1/fake_patient1_doc10_NOTE.txt").read()
     # entities = rag_chain.get_serialized_entities(text)
     # print(entities)

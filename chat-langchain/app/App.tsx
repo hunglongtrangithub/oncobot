454166bc492d213bc472f5422a4fa7757f8435ec
Hung Tran <hunglongtran2004@gmail.com>
2024-07-25 15:48:01 -0400

    feat(App): use less extreme colors for dark/light mode
    Former-commit-id: b11e57efa77afedf5d9c0b4ea00d001cdb38941e

--- chat-langchain/app/App.tsx
+++ chat-langchain/app/App.tsx
@@ -1,10 +1,10 @@
-import { ChatWindow } from "../app/components/ChatWindow";
+import { ChatWindow } from "./components/ChatWindow";
 import { ToastContainer } from "react-toastify";
 import { useToken } from "@chakra-ui/react";
 
 export default function App() {
   const title = "Medical Chatbot";
-  const [light, dark] = useToken("colors", ["gray.100", "gray.900"]);
+  const [light, dark] = useToken("colors", ["gray.200", "gray.800"]);
   return (
     <>
       <ToastContainer
